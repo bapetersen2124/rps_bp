30df27789e84abd1e6875df4d3703cd4bc63964c
bapetersen2124 <bapetersen2124@gmail.com>
2017-10-03 20:59:55 -0400

    first commit of rock_paper_scissors

---
+++ javascript/practice.js
@@ -0,0 +1,14 @@
+/*
+var consumedCups = 4
+
+if (consumedCups < 3) {
+  console.log("Yes, I'll take another cup of coffee!")
+}
+else {
+  console.log("I think I'm okay for now")
+}
+*/
+
+var temp = 80
+
+console.log("The temperature is " + temp + " degrees.");
---
+++ rock_paper_scissors.js
@@ -0,0 +1,6 @@
+var playerOne = "Brooke";
+var playerTwo = "Computer";
+
+var weapons = ['rock' , 'paper', 'scissors'];
+
+var weaponOfChoice = weapons[parseInt(Math.random()*weapons.length) %3];
